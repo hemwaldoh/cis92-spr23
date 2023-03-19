6e4f7f497e308a8f14f86690a1e12e892abea5a1
Lisa Remoaldo <lmremoaldo@gmail.com>
2023-03-19 16:25:45 +0000

    working on db default user

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-6xoule5vcp9+r6+ww18t6a4+wix0)+(s_k+d6)9@_v+@8m89pa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
