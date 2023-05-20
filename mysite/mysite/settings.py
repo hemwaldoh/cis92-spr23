1cb4048e01fe93707624b86f3d1facede3b335d0
Lisa Remoaldo <lmremoaldo@gmail.com>
2023-05-20 15:49:15 +0000

    prep for cloud run

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6xoule5vcp9+r6+ww18t6a4+wix0)+(s_k+d6)9@_v+@8m89pa'
+# SECRET_KEY = 'django-insecure-6xoule5vcp9+r6+ww18t6a4+wix0)+(s_k+d6)9@_v+@8m89pa'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = [ "https://*.run.app" ]
 
 # Application definition
 
